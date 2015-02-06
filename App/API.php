6114805c88c023fe7b2f51aa275a24e4da34433f
robert <robrotheram@gmail.com>
2015-02-06 12:20:20 +0000

    link on api

--- App/API.php
+++ App/API.php
@@ -15,30 +15,36 @@ include 'DateCalculator.php';
 switch ($_POST['funtion']) {
     case "ADD":
         $date1 = createNewDate($_POST['dateDay1'], $_POST['dateMonth1'], $_POST['dateYear1']);
-        echo "add";
+        echo "<br> Result:";
+        echo "<br>";
         echo date_format(addAmount($date1, $_POST['dateDay2'], $_POST['dateMonth2'], $_POST['dateYear2']), "d/m/Y");
         break;
     case "DIFF":
         $date1 = createNewDate($_POST['dateDay1'], $_POST['dateMonth1'], $_POST['dateYear1']);
         $date2 = createNewDate($_POST['dateDay2'], $_POST['dateMonth2'], $_POST['dateYear2']);
-        echo "<br> Diff Check:";
+        echo "<br> Result:";
         echo "<br>";
         echo diffDate($date1, $date2);
         break;
     case "TIMETILLNOW":
         $date1 = createNewDate($_POST['dateDay1'], $_POST['dateMonth1'], $_POST['dateYear1']);
         $date2 = createNewDate($_POST['dateDay2'], $_POST['dateMonth2'], $_POST['dateYear2']);
-        echo "TimeIsNowCheck";
+        echo "<br> Result:";
+        echo "<br>";
         echo diffDate(timeIsNow(), $date2);
         break;
     case "DAYDATTE":
         $date1 = createNewDate($_POST['dateDay1'], $_POST['dateMonth1'], $_POST['dateYear1']);
         $date2 = createNewDate($_POST['dateDay2'], $_POST['dateMonth2'], $_POST['dateYear2']);
+        echo "<br> Result:";
+        echo "<br>";
         echo dayOfWeek($date1);
         echo "<br/>";
         echo dayOfWeek($date2);
         break;
 }
+
+echo "<a href='../index.php'>Back to calulator page</a>";
 
 /* todo Add better ech statements  -> turn to json api front page will use ajax */
 
